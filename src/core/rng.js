c973022ca764b66e0bd7eebf95e48c4c4fdc6264
ethan <95717922+popshuv@users.noreply.github.com>
2026-03-20 16:49:56 -0400

    Delete src directory

--- src/core/rng.js
+++
@@ -1,9 +0,0 @@
-function createRng(seed = Date.now()) {
-  let state = (seed >>> 0) || 1;
-  return () => {
-    state = (1664525 * state + 1013904223) >>> 0;
-    return state / 0x100000000;
-  };
-}
-
-module.exports = { createRng };
